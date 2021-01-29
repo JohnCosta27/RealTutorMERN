65a642592f09a125501777bb10bf10f5d2ace8d1
JohnCosta27 <johncosta027@gmail.com>
2021-01-29 20:53:13 +0000

    Still testing

--- server.js
+++ server.js
@@ -9,12 +9,28 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening on port: " + port));
 
-const studentsRoute = require('./routes/students.js');
-app.use('/students', studentsRoute);
+//const studentsRoute = require('./routes/students.js');
+//app.use('/students', studentsRoute);
 
 app.get('/', (req, res) => {
     res.send("Hello world");
 })
+
+const students = express.Router();
+const Student = require('./models/Student');
+
+app.use('/students', students);
+
+students.get('/readall', async (req, res) => {
+
+    try {
+        const students = await Student.find();
+        res.json(students);
+    } catch (err) {
+        res.json({error: err})
+    }
+
+});
 
 mongoose.connect(process.env.DB_CONNECTION,  {useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log("Connected to MongoDB");
