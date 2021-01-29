c36d590e7658546c12852b3595dab7176f102a87
JohnCosta27 <johncosta027@gmail.com>
2021-01-29 20:48:38 +0000

    ?

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Student = require('./models/Student');
 require('dotenv/config');
 
 app.use(bodyParser.json());
@@ -13,13 +12,8 @@ app.listen(port, () => console.log("Listening on port: " + port));
 const studentsRoute = require('./routes/students.js');
 app.use('/students', studentsRoute);
 
-app.get('/', async (req, res) => {
-    try {
-        const students = await Student.find();
-        res.json(students);
-    } catch (err) {
-        res.json({error: err})
-    }
+app.get('/', (req, res) => {
+    res.send("Hello world");
 })
 
 mongoose.connect(process.env.DB_CONNECTION,  {useNewUrlParser: true, useUnifiedTopology: true}, () => {
