af5bae40a15cdb7fe3441c219be827d328919ba4
JohnCosta27 <johncosta027@gmail.com>
2021-01-29 19:55:19 +0000

    Added default request

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(bodyParser.json());
 const studentsRoute = require('./routes/students.js');
 app.use('/students', studentsRoute);
 
+app.get('/', (req, res) => {
+    res.send("Hello world");
+})
+
 mongoose.connect(process.env.DB_CONNECTION,  {useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log("Connected to MongoDB");
 });
