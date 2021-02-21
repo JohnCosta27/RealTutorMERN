179d33e17e7b404c3b9e4b7a9078fa2224d1195c
John <johncosta027@gmail.com>
2021-02-21 21:57:14 +0000

    Kept working on the grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { grey, blueGrey, teal } from '@material-ui/core/colors';
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
@@ -17,18 +17,26 @@ const theme = createMuiTheme({
       main: blueGrey[500],
       background: teal[900],
       light: grey[100],
-      text: '#DDDDDD'
+      text: '#DDDDDD',
+      cards: grey[500]
     }
+  },
+typography: {
+  h1: {
+    fontSize: "5em",
+    color: grey[900]
   }
-});
+}});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
-      <Route exact path="/" render={(props) => (<Dashboard theme={theme}></Dashboard>)}></Route>
-      <Route path="/login" render={(props) => (<Login theme={theme}></Login>)}></Route>
-      <Route path="/studentdashboard" render={(props) => (<StudentDashboard theme={theme}></StudentDashboard>)}></Route>
+    <Route exact path="/" render={(props) => (<Dashboard theme={theme}></Dashboard>)}></Route>
+    <Route path="/login" render={(props) => (<Login theme={theme}></Login>)}></Route>
+    <Route path="/studentdashboard" render={(props) => (<StudentDashboard theme={theme}></StudentDashboard>)}></Route>
     </Router>
+    </ThemeProvider>
     );
   }
   
--- client/src/components/pages/StudentDashboard.js
+++ client/src/components/pages/StudentDashboard.js
@@ -4,17 +4,25 @@ import '../../App.css';
 import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import { Typography } from '@material-ui/core';
 
 function StudentDashboard(props) {
     
     const theme = createMuiTheme(props.theme);
     
     const useStyles = makeStyles((theme) => ({
+        studentBox: {
+            display: "flex",
+            flexDirection: "column"
+        },
         studentNameWrapper: {
             flex: 1
         },
         gridWrapper: {
-            flex: 5
+            flex: 5,
+            height: "100%"
         },
         studentNameText: {
             textAlign: "center"
@@ -22,40 +30,71 @@ function StudentDashboard(props) {
         paper: {
             margin: 5,
             textAlign: 'center',
-            backgroundColor: theme.palette.primary.background,
-            height: 200
+            height: 350,
+            borderRadius: 10
+        },
+        studentBoxWrapper: {
+            width: "100%",
+            marginTop: 50,
+            marginLeft: 10,
+            marginRight: 10
+        },
+        cardsWrapper: {
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column"
+        },
+        cardContent: {
+            flex: 4
+        },
+        cardAction: {
+            flex: 1
         }
     }));
     
     const classes = useStyles();
-    
+
     return (
         <div className="App">
         <div className="leftNav" style={{backgroundColor: theme.palette.primary.background}}></div>
         <div className="mainWrapper" style={{backgroundColor: theme.palette.primary.main}}>
-        <div className="studentBoxWrapper">
+        <div className={classes.studentBoxWrapper}>
         
+        <Box className={classes.studentBox}>
+
         <div className={classes.studentNameWrapper}>
-        <h1 className={classes.studentNameText}>Hello World</h1>
+            <Typography variant="h1">Student name</Typography>
         </div>
-        <div className={classes.gridWrapper}>
         
         <Grid container>
-            <Grid item xs={6}>
-                <Paper className={classes.paper}>xs=6</Paper>
-            </Grid>
-            <Grid item xs={6}>
-                <Paper className={classes.paper}>xs=6</Paper>
-            </Grid>
-            <Grid item xs={6}>
-                <Paper className={classes.paper}>xs=6</Paper>
-            </Grid>
-            <Grid item xs={6}>
-                <Paper className={classes.paper}>xs=6</Paper>
-            </Grid>
+        <Grid item xs={6}>
+        <Box className={classes.paper} style={{backgroundColor: theme.palette.primary.cards}}>
+        <Box display="flex" className={classes.cardsWrapper}>
+            <Typography variant="h2">Lessons</Typography>
+            <Box className={classes.cardContent}></Box>
+            <Box className={classes.cardAction}></Box>
+        </Box>
+        </Box>
+        </Grid>
+        <Grid item xs={6}>
+        <Box className={classes.paper} style={{backgroundColor: theme.palette.primary.cards}}>
+        <Typography variant="h2">Upcoming</Typography>
+        </Box>
+        </Grid>
+        <Grid item xs={6}>
+        <Box className={classes.paper} style={{backgroundColor: theme.palette.primary.cards}}>
+        <Typography variant="h2">Progress</Typography>
+        </Box>
+        </Grid>
+        <Grid item xs={6}>
+        <Box className={classes.paper} style={{backgroundColor: theme.palette.primary.cards}}>
+        <Typography variant="h2">Details</Typography>
+        </Box>
+        </Grid>
         </Grid>
         
-        </div>
+        </Box>
         
         </div>
         </div>
