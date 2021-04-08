76c4f4fad58bd30b74a6d69f8086ad69dec830ed
John Costa <johncosta027@gmail.com>
2021-04-08 15:56:11 +0100

    env

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(cookieParser());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening on port: " + port));
 
 const accountsRoute = require('./routes/accounts.js');
