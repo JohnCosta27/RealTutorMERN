efa38be85cf755173bd026f9541d6f8ef32b0b16
John <johncosta027@gmail.com>
2021-05-27 16:16:25 +0100

    Deploying commit, really small change

--- client/src/components/FeatureComponents/StudentList.js
+++ client/src/components/FeatureComponents/StudentList.js
@@ -1,61 +1,64 @@
-import React, { useState, useEffect } from "react";
-import StudentCard from "../general/StudentCard";
+import React, { useState, useEffect } from 'react';
+import StudentCard from '../general/StudentCard';
 
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 const StudentList = (props) => {
-  const [students, setStudents] = useState([]);
+	const [students, setStudents] = useState([]);
 
-  useEffect(() => {
-    getTutorStudents();
-  }, []);
+	useEffect(() => {
+		getTutorStudents();
+	}, []);
 
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
 
-  const getTutorStudents = async () => {
-    const response = await fetch(
-      "/accounts/gettutorstudents?tutorid=" + urlParams.get("tutorid"),
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-      }
-    );
+	const getTutorStudents = async () => {
+		const response = await fetch(
+			'/accounts/gettutorstudents?tutorid=' + urlParams.get('tutorid'),
+			{
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				credentials: 'include',
+			}
+		);
 
-    const data = await response.json();
-    console.log(data);
+		const data = await response.json();
+		console.log(data);
+		try {
+			setStudents(data.students);
+		} catch (error) {
 
-    setStudents(data.students);
-  };
+		}
+	};
 
-  const useStyles = makeStyles((theme) => ({
-    studentListWrapper: {
-      display: "flex",
-      flexDirection: "column",
-      width: "50%",
-      [theme.breakpoints.down('sm')]: {
-        width: "90%"
-      }
-    },
-  }));
+	const useStyles = makeStyles((theme) => ({
+		studentListWrapper: {
+			display: 'flex',
+			flexDirection: 'column',
+			width: '50%',
+			[theme.breakpoints.down('sm')]: {
+				width: '90%',
+			},
+		},
+	}));
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <div className={classes.studentListWrapper}>
-      {students.map((student) => (
-        <StudentCard
-          firstname={student.firstname}
-          surname={student.surname}
-          id={student._id}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className={classes.studentListWrapper}>
+			{students.map((student) => (
+				<StudentCard
+					firstname={student.firstname}
+					surname={student.surname}
+					id={student._id}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default StudentList;
